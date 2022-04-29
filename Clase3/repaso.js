2356d8520bc9d1d1ab1c937835a8937df70bcaa6
Sh4rkD <20006132@es.uveg.edu.mx>
2022-04-28 21:54:41 -0600

    clase 3 y 4

---
+++ Clase3/repaso.js
@@ -0,0 +1,15 @@
+function sumarDosNumeros(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+/**
+ * 
+ * @param {number} numero1 Primer numero a sumar
+ * @param {number} numero2 Segundo numero a sumar
+ * @returns {number} El resultado de la suma
+ */
+function sumarDosNumerosDocumentada(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+console.log(sumarDosNumerosDocumentada(1,2));
